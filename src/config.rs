ed995a0d1ba31a7badfdffeb1b067610ffd46b8f
lazoliver <wilian.oliveiractt@gmail.com>
2024-01-17 10:41:57 -0300

    feat/config

---
+++ src/config.rs
@@ -0,0 +1,35 @@
+use dotenv::dotenv;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ReleaseMode {
+    Dev,
+    Prod
+}
+
+fn default_api_port() -> u16 {
+    4000
+}
+
+fn default_timeout() -> u64 {
+    75
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    #[serde(default = "default_api_port")]
+    pub api_port: u16,
+    #[serde(default = "default_timeout")]
+    pub timeout: u64,
+    pub release_mode: ReleaseMode
+}
+
+impl Config {
+    pub fn new() -> Self {
+        dotenv().ok();
+        let config = envy::from_env::<Config>().expect("Error processing config object");
+
+        config
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-<<<<<<< HEAD
 #[macro_use] extern crate rocket;
 
 mod handlers;
+mod config;
+
+use crate::config::Config;
 
 use crate::handlers::utils::{health_handler, echo_handler};
 
 #[launch]
 fn rocket() -> _ {
+    let config = Config::new();
+
     rocket::build()
+        .configure(rocket::Config::figment().merge(("port", config.api_port)))
         .mount("/", routes![health_handler])
         .mount("/", routes![echo_handler])
 }
-=======
-fn main() {
-    println!("Hello, world!");
-}
->>>>>>> master
