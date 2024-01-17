af6f20f55030c31cbb645e700429c5b1a2e3a0c7
lazoliver <wilian.oliveiractt@gmail.com>
2024-01-17 14:06:34 -0300

    remove-unused-fields

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,14 @@
 use dotenv::dotenv;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub enum ReleaseMode {
-    Dev,
-    Prod
-}
-
 fn default_api_port() -> u16 {
     4000
-}
-
-fn default_timeout() -> u64 {
-    75
 }
 
 #[derive(Deserialize)]
 pub struct Config {
     #[serde(default = "default_api_port")]
     pub api_port: u16,
-    #[serde(default = "default_timeout")]
-    pub timeout: u64,
-    pub release_mode: ReleaseMode
 }
 
 impl Config {
