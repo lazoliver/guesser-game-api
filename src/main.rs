10628cb22caed4a3a0e1d54be92998ab8c2682e5
lazoliver <wilian.oliveiractt@gmail.com>
2024-02-15 21:48:18 -0300

    feat/guess-secret

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod error;
 
 use crate::config::Config;
 use crate::storage::storage::{Storage, AttemptCountRule};
+use crate::storage::secret::NewSecret;
 use crate::config::ReleaseMode;
 
 use crate::handlers::utils::{health_handler, echo_handler, full_health_handler};
@@ -26,6 +27,17 @@ async fn rocket() -> _ {
     };
 
     let storage = Storage::new(config.mongo_uri).await.expect("Error to connecting database");
+
+    let new_secret = NewSecret {
+        secret: "Futebol".to_string(),
+        clue1: "Pés".to_string(),
+        clue2: "22".to_string(),
+        clue3: "Bola".to_string()
+    };
+
+    let result = storage.create_secret(new_secret).await.expect("Deu ruim fi!");
+
+    println!("{:?}", result);
 
     let default_level = match config.release_mode {
         ReleaseMode::Dev => "debug",
--- src/storage/secret.rs
+++ src/storage/secret.rs
@@ -4,9 +4,11 @@ use serde::{Serialize, Deserialize};
 use mongodb::{bson::doc, results::InsertOneResult};
 use uuid::Uuid;
 
+use sha3::{Digest, Keccak256};
+
 use super::storage::{AttemptCountRule, Storage};
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct SecretEntity {
     pub id: Uuid,
     pub secret: String,
@@ -25,21 +27,17 @@ pub struct NewSecret {
     pub clue3: String
 }
 
-pub struct Secret {
-    pub id: Uuid,
-    pub guessed: bool,
-    pub clue1: Option<String>,
-    pub clue2: Option<String>,
-    pub clue3: Option<String>,
-    pub guesser: Option<String>,
-    pub secret: Option<String>
-}
-
 impl Storage {
-    pub async fn create_secret(&self, new_secret: NewSecret, attempt_rule: AttemptCountRule) -> Result<Secret, AppError> {
+    pub async fn create_secret(&self, new_secret: NewSecret) -> Result<SecretEntity, AppError> {
+        let mut hasher = Keccak256::new();
+
+        hasher.update(new_secret.secret);
+
+        let hashed_secret = hex::encode(hasher.finalize().to_vec());
+
         let secret = SecretEntity {
             id: Uuid::new_v4(),
-            secret: new_secret.secret,
+            secret: hashed_secret,
             clue1: new_secret.clue1,
             clue2: new_secret.clue2,
             clue3: new_secret.clue3,
@@ -52,7 +50,7 @@ impl Storage {
 
         let created_secret = self.get_secret_entity(secret.id).await?;
 
-        Ok(self.process_secret(attempt_rule, created_secret))
+        Ok(created_secret)
     }
 
     pub async fn get_secret_entity(&self, secret_id: Uuid) -> Result<SecretEntity, AppError> {
@@ -63,24 +61,11 @@ impl Storage {
         }
     }
 
-    pub fn process_secret(&self, attempt_rule: AttemptCountRule, entity: SecretEntity) -> Secret {
-        Secret {
-            id: entity.id,
-            guessed: entity.guesser.is_some(),
-            clue1: match (entity.guess_attempts >= attempt_rule.clue1_attempts) {
-                true => Some(entity.clue1),
-                false => None
-            },
-            clue2: match (entity.guess_attempts >= attempt_rule.clue2_attempts) {
-                true => Some(entity.clue2),
-                false => None
-            },
-            clue3: match (entity.guess_attempts >= attempt_rule.clue3_attempts) {
-                true => Some(entity.clue3),
-                false => None
-            },
-            secret: entity.guessed_secret,
-            guesser: entity.guesser
-        }
+    pub async fn guess_secret(&self, secret_id: Uuid, guess: String, username: String) {
+        // 1. Pegar segredo do banco;
+        // 2. Verificar guesser, se existir retornar erro, se não seguir, AppError<AlreadyGuessed>;
+        // 3. Fazer hash/encode da guess;
+        // 4. Checar se a hash de guess e o secret são iguais, se sim salva no banco de dados o SecretEntity com Guesser e Guessed_Secret e retonra o segredo;
+        // 5. Se não forem iguas as hashs de Guess e Secret, aumentar o contador de attempts e retorna o segredo;
     }
 }
